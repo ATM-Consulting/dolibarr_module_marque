b787284fdc2c8c9fc59b2bd94f4a1f7bd8fa6810
ATM-nicolasV <nicolas.vidal@atm-consutling.fr>
2024-10-03 17:20:32 +0200

    fix de la restriction du champ marque utilisé à la création d'un objet si la conf correspondante est renseignée

--- class/actions_marque.class.php
+++ class/actions_marque.class.php
@@ -113,37 +113,6 @@ class ActionsMarque extends marque\RetroCompatCommonHookActions
 		$sourcecompany = &$mysoc;
 		$sourceconf = &$conf;
 
-//		dol_include_once('/multicompany/class/dao_multicompany.class.php');
-//
-//		$dao = new DaoMulticompany($db);
-//		$dao->fetch($entity);
-//
-//		$sourceconf->mycompany->dir_output= DOL_DATA_ROOT;
-//		if($entity>1)$sourceconf->mycompany->dir_output.='/'.$entity.'/mycompany';
-//		else $sourceconf->mycompany->dir_output.='/mycompany';
-//
-//		$sourcecompany->nom = $sourcecompany->name = $dao->MAIN_INFO_SOCIETE_NOM;
-//		$sourcecompany->town = $dao->MAIN_INFO_SOCIETE_TOWN;
-//		$sourcecompany->zip = $dao->MAIN_INFO_SOCIETE_ZIP;
-//		$sourcecompany->state = $dao->MAIN_INFO_SOCIETE_STATE;
-//		$sourcecompany->logo = $dao->MAIN_INFO_SOCIETE_LOGO;
-//		$sourcecompany->logo_small = $dao->MAIN_INFO_SOCIETE_LOGO_SMALL;
-//		$sourcecompany->logo_mini = $dao->MAIN_INFO_SOCIETE_LOGO_MINI;
-//
-//		$sourcecompany->url = $dao->MAIN_INFO_SOCIETE_WEB;
-//		$sourcecompany->address = $dao->MAIN_INFO_SOCIETE_ADDRESS;
-//		$sourcecompany->phone = $dao->MAIN_INFO_SOCIETE_TEL;
-//		$sourcecompany->email = $dao->MAIN_INFO_SOCIETE_MAIL;
-//		$sourcecompany->fax = $dao->MAIN_INFO_SOCIETE_FAX;
-//		$sourcecompany->managers = $dao->MAIN_INFO_SOCIETE_MANAGERS;
-//		$sourcecompany->capital = $dao->MAIN_INFO_CAPITAL;
-//		$sourcecompany->typent_id = $dao->MAIN_INFO_SOCIETE_FORME_JURIDIQUE;
-//		$sourcecompany->idprof1 = $dao->MAIN_INFO_SIREN;
-//		$sourcecompany->idprof2 = $dao->MAIN_INFO_SIRET;
-//		$sourcecompany->idprof3 = $dao->MAIN_INFO_APE;
-//		$sourcecompany->idprof4 = $dao->MAIN_INFO_RCS;
-//		$sourcecompany->intra_vat = $dao->MAIN_INFO_TVAINTRA;
-
         $fk_entity_origin = $sourceconf->entity;
 		$sourceconf->entity = $entity;
 		foreach ($sourceconf->global as $attr => &$value)
@@ -185,7 +154,7 @@ class ActionsMarque extends marque\RetroCompatCommonHookActions
 		$TContext = explode(':', $parameters['context']);
 		if (in_array('globalcard',$TContext))
 		{
-			if(!empty(getDolGlobalString('MARQUE_ENTITIES_LINKED_'.$conf->entity)) && (GETPOST('attribute') === 'entity_marque' || $action=='edit' ) ) {
+			if(!empty(getDolGlobalString('MARQUE_ENTITIES_LINKED_'.$conf->entity)) && (GETPOST('attribute') === 'entity_marque' || $action=='edit' || $action=='create') ) {
 				?>
 				<script type="text/javascript">
 				$(document).ready( function () {
